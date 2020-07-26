ca841c14c479e81a0825dbab0de73b6d10afe273
Paritosh Batish <batishparitosh2@gmail.com>
2020-07-26 23:25:23 +0530

    0.1 final

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -10,6 +10,7 @@ class DLL {
   }
 
   Node head;
+  Node tail = null;
 
   public void insertAtBeginning(int data) {
     Node newNode = new Node(data);
@@ -40,14 +41,56 @@ class DLL {
 
   }
 
+  public void insertAfter(Node prevNode, int data) {
+    if (prevNode == null) {
+      System.out.println("Error");
+      return;
+    }
+    Node newNode = new Node(data);
+
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+    newNode.previous = prevNode;
+    newNode.next.previous = newNode;
+  }
+
+  public void insertBefore(Node nextNode, int data) {
+
+  }
+
+  public void deleteAtBeginning() {
+    if (head == null) {
+      System.out.println("List is Empty");
+      return;
+    } else {
+      if (head != tail) {
+        Node nextOfHead = head.next;
+        head = null;
+        head = nextOfHead;
+        head.previous = null;
+      } else {
+        head = tail = null;
+      }
+    }
+  }
+
   public void show() {
     Node n = head;
+    Node last = null;
 
-    while (n.next != null) {
-      System.out.print(n.data + ", ");
+    while (n != null) {
+      System.out.print(n.data + " ");
+      last = n;
       n = n.next;
     }
-    System.out.println(n.data);
+    System.out.println();
+
+    // Reverse
+    while (last != null) {
+      System.out.print(last.data + " ");
+      last = last.previous;
+    }
+    System.out.println("(Reverse)");
   }
 
 }
@@ -62,6 +105,9 @@ public class DoublyLinkedList {
     list.show();
 
     list.insertAtBeginning(22);
+    list.show();
+
+    list.insertAfter(list.head.next, 33);
     list.show();
   }
 
